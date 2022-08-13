7022b32ad58664800859a47d14264b221322d328
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-08-13 21:49:02 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,10 @@
+#Sting in python
+#fun with String in python
+
+print("python is fun")
+
+#string with escape character
+# "\" is special character call 'escape character'
+# used to represent certine things in a like new line ,tab etc.
+
+print("it\'s my python file \n I love python\n  I\'m learning python \n python is :\"COOL!\".")
