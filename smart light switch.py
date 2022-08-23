0f0ae26bfb1ab6080aa9ca9bd1d9dd93536d7a9e
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-08-23 21:21:59 +0530

    Add files via upload

---
+++ smart light switch.py
@@ -0,0 +1,11 @@
+#Smart  light switch
+#Switches the light off if it's day time and switch on in night time 
+
+is_day = False
+light_on = not is_day
+
+print("Day time?")
+print(is_day)
+
+print("Lights on?")
+print(light_on)
