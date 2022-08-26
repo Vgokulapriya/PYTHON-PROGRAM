e1097fdc73434c458744f8bcc2d745e55e360c70
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-08-26 19:48:48 +0530

    Add files via upload

---
+++ Track sales data.py
@@ -0,0 +1,16 @@
+# Track sales data
+# Seeing retailer hit their sale traget
+# Checking their are product still in stock
+
+stock = int(input())
+product_sold = int(input())
+target = int(input()) 
+
+target_hit = product_sold == target
+print( "hit product sale target:" )
+print(target_hit)
+
+current_stock = stock - product_sold
+in_stock = current_stock != 0
+print("product in stock :")
+print(in_stock)
