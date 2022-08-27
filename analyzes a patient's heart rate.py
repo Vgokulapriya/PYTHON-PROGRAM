82110cdb3374fe791e83506dcde0140a915b5dbd
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-08-27 21:09:18 +0530

    Add files via upload

---
+++ analyzes a patient's heart rate.py
@@ -0,0 +1,15 @@
+#program that analyzes a patient's heart rate.
+
+heart_rate = int(input())
+
+too_low = heart_rate < 60
+too_high = heart_rate > 100
+normal = heart_rate in  range(60,100)
+print("Low Heart rate:")
+print(too_low)
+
+print("High Heart rate:")
+print(too_high)
+
+print("normal heart rate:")
+print(normal)
