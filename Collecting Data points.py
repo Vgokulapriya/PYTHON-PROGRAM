1ae355704a39b4a2b63851d355f4ca5a9dc4b8f7
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-08-29 20:59:18 +0530

    Add files via upload

---
+++ Collecting Data points.py
@@ -0,0 +1,10 @@
+# collecting Data points.
+# program to check if there are enough data points within a dataset.
+ 
+minimum_points = int(input())
+data_points = int(input())
+
+if data_points >= minimum_points:
+    print("There are engough data points!")
+if data_points < minimum_points:
+    print("keeping collecting data.")
