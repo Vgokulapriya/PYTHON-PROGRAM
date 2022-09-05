bc975bbe8af341345715947d30df890da9547130
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-09-05 21:33:17 +0530

    Add files via upload

---
+++ currency convertor.py
@@ -0,0 +1,14 @@
+# currency convertor
+def main():
+    print("convert US dollars to pounds sterling")
+    print()
+
+    dollars = eval(input("Enter the amonut in Dollars :"))
+
+    pounds = convert_to_pounds(dollars)
+
+    print("That is",pounds,"pounds.")
+
+convert_to_pounds = lambda dollars: dollars * 0.82
+
+main()
