27d7642e4de65829f997934c5146a8a2d2e309d1
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-10-05 21:02:47 +0530

    Added Linear search.py

---
+++ Linear search.py
@@ -0,0 +1,17 @@
+lst = []
+num = int(input("Enter the size of list :-"))
+for n in range(num):
+    numbers = int(input("Enter the array of %d element :-" % n))
+    lst.append(numbers)
+x = int(input("Enter number to search in list :-"))
+i = 0
+flag = False
+while i < len(lst):
+    if lst[i] == x:
+        flag = True
+        break
+    i = i + 1
+if flag == 1:
+    print("{} was found at index {}.".format(x, i))
+else:
+    print("{} was not found.".format(x))
