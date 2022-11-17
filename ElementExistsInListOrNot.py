2791009f74060d4f586950a5278a5d59331e30a4
Vgokulapriya <110735097+Vgokulapriya@users.noreply.github.com>
2022-11-17 16:37:49 +0530

    Added ElementExistsInList.py

---
+++ ElementExistsInListOrNot.py
@@ -0,0 +1,10 @@
+# python code to Check if element exists in list or not
+
+lst = [1, 6, 3, 5, 3, 4]
+i = int(input("Enter the number:"))
+# if element present then return
+# exist otherwise not exist
+if i in lst:
+    print("exist")
+else:
+    print("not exist")
